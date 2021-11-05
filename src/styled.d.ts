07480241f54a40c0fa60adcf8b585e9b9add4fad
GW <kowonp@gmail.com>
2021-11-05 18:09:16 +0900

    Declare DefaultTheme Interface

---
+++ src/styled.d.ts
@@ -0,0 +1,10 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    redColor: string;
+    blueColor: string;
+    greenColor: string;
+    orangeColor: string;
+  }
+}
